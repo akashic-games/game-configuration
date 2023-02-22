05fd449d038572e45976d7e8769a27eb8826c6e9
dera- <alchemy.and.fairy@gmail.com>
2023-02-22 20:48:47 +0900

    reflect pr comment

--- src/__tests__/extractAssetPaths.spec.ts
+++ src/__tests__/extractAssetPaths.spec.ts
@@ -15,32 +15,50 @@ describe("extractAssetPaths", () => {
 			},
 			globalScripts: ["./node_modules/foo/bar.js"]
 		};
-		const audioExtensionResolver = (_asset: AudioAssetConfigurationBase): string[] => [];
-		const result = extractAssetPaths({ gameConfiguration, audioExtensionResolver });
+		const result = extractAssetPaths({ gameConfiguration });
 		expect(result).toEqual(["script/main.js", "script/mainScene.js", "image/chara.png", "./node_modules/foo/bar.js"]);
 	});
-	it("指定されたGameConfigurationに音声アセットが登録されている場合、指定された関数に従って音声ファイルパスが取得できる", () => {
+	it("指定されたGameConfigurationに音声アセットが登録されている場合、指定された audioExtensionResolver に従って音声ファイルパスが取得できる", () => {
 		const gameConfiguration: GameConfiguration = {
 			width: 320,
 			height: 240,
 			main: "./script/main.js",
 			assets: {
 				main: { type: "script", path: "script/main.js", global: true },
-				se1: { type: "audio", path: "audio/se1", duration: 1000, systemId: "sound" },
-				// hint.extensionsが指定されている場合は、関数ではなくこちらの値が優先される
-				se2: { type: "audio", path: "audio/se2", duration: 1000, systemId: "sound", hint: { extensions: ["m4a", "aac"] } }
+				se: { type: "audio", path: "audio/se", duration: 1000, systemId: "sound" }
 			},
 			globalScripts: ["./node_modules/foo/bar.js"]
 		};
-		const audioExtensionResolver = (_asset: AudioAssetConfigurationBase): string[] => ["ogg", "aac"];
+		const audioExtensionResolver = (_asset: AudioAssetConfigurationBase): string[] => ["m4a", "ogg", "aac"];
 		const result = extractAssetPaths({ gameConfiguration, audioExtensionResolver });
-		expect(result).toEqual([
-			"script/main.js",
-			"audio/se1.ogg",
-			"audio/se1.aac",
-			"audio/se2.m4a",
-			"audio/se2.aac",
-			"./node_modules/foo/bar.js"
-		]);
+		expect(result).toEqual(["script/main.js", "audio/se.m4a", "audio/se.ogg", "audio/se.aac", "./node_modules/foo/bar.js"]);
+	});
+	it("audioExtensionResolver が指定されない場合、hint.extensionsに従って音声ファイルパスが取得できる", () => {
+		const gameConfiguration: GameConfiguration = {
+			width: 320,
+			height: 240,
+			main: "./script/main.js",
+			assets: {
+				main: { type: "script", path: "script/main.js", global: true },
+				se: { type: "audio", path: "audio/se", duration: 1000, systemId: "sound", hint: { extensions: ["m4a", "aac"] } }
+			},
+			globalScripts: ["./node_modules/foo/bar.js"]
+		};
+		const result = extractAssetPaths({ gameConfiguration });
+		expect(result).toEqual(["script/main.js", "audio/se.m4a", "audio/se.aac", "./node_modules/foo/bar.js"]);
+	});
+	it("audioExtensionResolver が指定されず音声アセット情報にhint.extensionsが無い場合、oggとaacの音声ファイルパスが取得できる", () => {
+		const gameConfiguration: GameConfiguration = {
+			width: 320,
+			height: 240,
+			main: "./script/main.js",
+			assets: {
+				main: { type: "script", path: "script/main.js", global: true },
+				se: { type: "audio", path: "audio/se", duration: 1000, systemId: "sound" }
+			},
+			globalScripts: ["./node_modules/foo/bar.js"]
+		};
+		const result = extractAssetPaths({ gameConfiguration });
+		expect(result).toEqual(["script/main.js", "audio/se.ogg", "audio/se.aac", "./node_modules/foo/bar.js"]);
 	});
 });
