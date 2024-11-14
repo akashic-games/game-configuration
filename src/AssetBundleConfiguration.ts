bce9467c39c9a151615e236f0a1a52a02fed9d6c
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2024-11-14 09:17:00 +0900

    fix: support `preload`

--- src/AssetBundleConfiguration.ts
+++ src/AssetBundleConfiguration.ts
@@ -1,10 +1,9 @@
 import type { ScriptAssetRuntimeValue } from "@akashic/pdi-types";
+import type { ScriptAssetConfigurationBase } from "./AssetConfiguration";
 
 export type BundledAssetConfiguration = BundledScriptAssetConfiguration;
 
-export interface BundledScriptAssetConfiguration {
-	type: "script";
-	path: string;
+export interface BundledScriptAssetConfiguration extends Omit<ScriptAssetConfigurationBase, "global" | "virtualPath" | "exports"> {
 	execute: (env: ScriptAssetRuntimeValue) => any;
 }
 
